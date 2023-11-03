b20d9959ed85b0e7b83b9cacc16b098155077d77
Dorota Maryniak <docia209aa@gmail.com>
2023-11-03 08:17:53 +0100

    Add NaN tests

---
+++ src/utils/convertPLNToUSD.js
@@ -0,0 +1,17 @@
+export const convertPLNToUSD = (PLN) => {
+
+ if (typeof(PLN) === 'string'){
+  return NaN;
+ }
+
+ else{
+  const PLNtoUSD = PLN / 3.5;
+
+  const formatter = new Intl.NumberFormat('en-US', {
+    style: 'currency',
+    currency: 'USD'
+  });
+
+  return formatter.format(PLNtoUSD).replace(/\u00a0/g, ' ');
+}
+}
